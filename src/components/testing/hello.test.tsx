2871249a1a05105cdf70d1012a0743ca571d0329
vilmosmisota <misotavilmos@gmail.com>
2022-06-23 15:29:16 +0100

    add and congifure lint

--- src/components/testing/hello.test.tsx
+++ src/components/testing/hello.test.tsx
@@ -1,6 +1,8 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Hello } from "./hello";
 
 it("renders hello", () => {
   render(<Hello />);
+  const myEl = screen.getByText(/Hello/);
+  expect(myEl).toBeInTheDocument();
 });
